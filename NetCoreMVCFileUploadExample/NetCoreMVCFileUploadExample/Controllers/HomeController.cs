6000c02613d940b6698db2b7d996cfead30dd3ed
ozhangulal <ozhangulal@outlook.com>
2020-06-20 18:01:36 +0300

    DotNetCoreMVCImageUploadExample
    easy dot net core image upload example.

---
+++ NetCoreMVCFileUploadExample/NetCoreMVCFileUploadExample/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCFileUploadExample.Models;
+
+namespace NetCoreMVCFileUploadExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        IHostingEnvironment _env;
+        string directory;
+        Random rnd = new Random();
+        public HomeController(IHostingEnvironment env)
+        {
+            _env = env;
+            directory = _env.ContentRootPath + "/Images/";
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SingleFile(IFormFile file)
+        {
+          
+            using (var fileStream = new FileStream(Path.Combine(directory,rnd.Next(0,99999).ToString()+".png"),FileMode.Create,FileAccess.Write))
+            {
+                file.CopyTo(fileStream);
+            }
+                return RedirectToAction("Index");
+        }
+
+        public IActionResult MultipleFiles(IEnumerable<IFormFile> files)
+        {
+            foreach (var file in files)
+            {
+                using (var fileStream = new FileStream(Path.Combine(directory, rnd.Next(0, 99999).ToString() + ".png"), FileMode.Create, FileAccess.Write))
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ NetCoreMVCFileUploadExample/NetCoreMVCFileUploadExample/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetCoreMVCFileUploadExample
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                services.AddMvc();
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
